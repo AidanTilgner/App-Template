e8bbf0837f7594f884d907e4d34c533c24127264
AidanTilgner <aidantilgner02@gmail.com>
2024-02-28 15:40:32 -0800

    feat: removed lorelens

--- client/components/Navigation/Navbar.tsx
+++ client/components/Navigation/Navbar.tsx
@@ -2,13 +2,7 @@ import React from "react";
 import styles from "./Navbar.module.scss";
 
 function Navbar() {
-  return (
-    <div className={styles.navbar}>
-      <div className={styles.logo}>
-        <img src="/assets/branding/logo.svg" alt="LoreLens logo." />
-      </div>
-    </div>
-  );
+  return <div className={styles.navbar}>I am the navbar</div>;
 }
 
 export default Navbar;
